142ae83ef74e2069a7a1749d36ba4861b83bd8ea
unikov226 <unikov226@gmail.com>
2026-09-08 15:46:57 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://uxxcmrewhohvhetfjlle.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://uxxcmrewhohvhetfjlle.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_44ZK_yp2muDR6QqpErQKJg_etMtYTUW";
 
 const supabase = window.supabase.createClient(
